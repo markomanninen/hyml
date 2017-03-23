e9e67bf4552a7b43ebbecd916eb96092ca26f05c
Marko Manninen <elonmedia@gmail.com>
2017-03-23 21:09:05 +0200

    update setup files

--- hyml/__init__.hy
+++ hyml/__init__.hy
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+(require (hyml.macros (*)))
+(import (hyml.macros (*)))
+(require (hyml.helpers (*)))
+(import (hyml.helpers (println indent)))
--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from distutils.core import setup
+
+install_requires = ['hy>=0.12.1']
+
 setup(
   name = 'hyml',
-  packages = ['hyml'], # this must be the same as the name above
-  version = 'v0.1',
+  
+  packages = ['hyml'],
+  package_dir = {'hyml': 'hyml'},
+  package_data = {
+    'hyml': ['*.hy']
+  },
+  
+  version = 'v0.1.1',
   description = 'HyML - XML / (X)HTML generator for Hy',
   author = 'Marko Manninen',
   author_email = 'elonmedia@gmail.com',
+
   url = 'https://github.com/markomanninen/hyml', # use the URL to the github repo
-  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.tar.gz', # I'll explain this in a second
-  keywords = ['hyland', 'python', 'markup language', 'dsl', 'xml', 'html', 'xhtml'], # arbitrary keywords
-  classifiers = [],
+  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.1.tar.gz', # I'll explain this in a second
+  keywords = ['hylang', 'python', 'markup language', 'dsl', 'xml', 'html', 'xhtml'], # arbitrary keywords
+  platforms = ['any'],
+  
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Lisp",
+    "Topic :: Software Development :: Libraries",
+  ]
 )
