2b240443b803e9acae7a6d150abe8c2aaeb9c970
Marko Manninen <elonmedia@gmail.com>
2017-03-23 20:43:06 +0200

    pip setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hyml',
+  packages = ['hyml'], # this must be the same as the name above
+  version = 'v0.1',
+  description = 'HyML - XML / (X)HTML generator for Hy',
+  author = 'Marko Manninen',
+  author_email = 'elonmedia@gmail.com',
+  url = 'https://github.com/markomanninen/hyml', # use the URL to the github repo
+  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.tar.gz', # I'll explain this in a second
+  keywords = ['hyland', 'python', 'markup language', 'dsl', 'xml', 'html', 'xhtml'], # arbitrary keywords
+  classifiers = [],
+)
