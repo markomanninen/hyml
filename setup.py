1f3630187d9ed8c9b7b1fbf08b347530ed80e9d7
Marko Manninen <elonmedia@gmail.com>
2017-03-23 21:28:19 +0200

    commenting requires and imports on init because of import failure
    it is common to get this error on import, if files are referenced in
    __init__.hy

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from distutils.core import setup
 
 install_requires = ['hy>=0.12.1']
 
+#python setup.py register -r pypitest
+#python setup.py register -r pypi
+# next command not recommended but what can you do in Windows...
+#python setup.py sdist upload -r pypitest
+#python setup.py sdist upload
+
 setup(
   name = 'hyml',
   
@@ -11,13 +17,13 @@ setup(
     'hyml': ['*.hy']
   },
   
-  version = 'v0.1.1',
+  version = 'v0.1.2',
   description = 'HyML - XML / (X)HTML generator for Hy',
   author = 'Marko Manninen',
   author_email = 'elonmedia@gmail.com',
 
   url = 'https://github.com/markomanninen/hyml', # use the URL to the github repo
-  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/markomanninen/hyml/archive/v0.1.2.tar.gz', # I'll explain this in a second
   keywords = ['hylang', 'python', 'markup language', 'dsl', 'xml', 'html', 'xhtml'], # arbitrary keywords
   platforms = ['any'],
   
