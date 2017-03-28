c8cf73859f0e7653b5a6fc6d8a25443338dbf12d
Marko Manninen <elonmedia@gmail.com>
2017-03-28 13:01:36 +0300

    new python module init

--- hyml/__init__.py
+++ hyml/__init__.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-#from .macros import *
-#from .helpers import *
+import hy
+from . minimal import *
+from hy.importer import hy_eval, import_buffer_to_hst
+
+def heval(tokens):
+    try:
+        return hy_eval(import_buffer_to_hst(tokens), {}, '<string>')
+    except Exception as e:
+    	print(e)
+
+def peval(tokens):
+	return heval("(print %s)" % tokens)
+
+def ml(tokens):
+	return heval("""
+(require [hyml.minimal [*]])
+(import (hyml.minimal (*)))
+(print (ml %s))
+""" % tokens)
