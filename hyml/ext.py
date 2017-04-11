1129d72a12968ba425f60568e1641fd4bbd0b74c
Marko Manninen <elonmedia@gmail.com>
2017-04-11 21:28:14 +0300

    providing empty comments and filename

--- hyml/ext.py
+++ hyml/ext.py
@@ -4,7 +4,7 @@ import hy, hy.importer as hyi
 from jinja2.ext import extract_from_ast
 import itertools
 
-def extract_from_ast(source, file=None):
+def extract_from_ast(source, file=""):
     d = None
     def filter_hy(e):
         global d
@@ -14,7 +14,7 @@ def extract_from_ast(source, file=None):
         elif not isinstance(e, hy.HySymbol) and isinstance(e, hy.HyString):
             if d == hy.HySymbol("_") or \
                d == hy.HySymbol("gettext"):
-                return file, d, e
+                return file, d, e, []
     return filter_hy(source)
 
 def chunks(l, n):
@@ -24,4 +24,4 @@ def chunks(l, n):
 def babel_extract(fileobj, *args, **kw):
     byte = fileobj.read()
     node = hyi.import_buffer_to_hst("".join(map(chr, byte)))[0]
-    return chunks(extract_from_ast(node), 3)
+    return chunks(extract_from_ast(node, fileobj.name), 4)
