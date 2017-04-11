a979fc29ae504ce3abe15d49664f0aa5074d8bce
Marko Manninen <elonmedia@gmail.com>
2017-04-11 16:58:21 +0300

    added base extensions

--- hyml/ext.py
+++ hyml/ext.py
@@ -3,10 +3,15 @@
 import os
 from jinja2.ext import babel_extract as extract_jinja2
 
+jinja_extensions = '''
+                    jinja2.ext.do, jinja2.ext.with_
+                   '''
+
 os.environ["VARIABLE_START_STRING"] = "(_ \""
 os.environ["VARIABLE_END_STRING"] = "\")"
 
 def babel_extract(fileobj, *args, **kw):
+	kw['options']['extensions'] = jinja_extensions
     fileobj.read()
     raw_extract = extract_jinja2(fileobj, *args, **kw)
     fileobj.seek(0)
