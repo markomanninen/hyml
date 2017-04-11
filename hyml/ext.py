a406a0fc3320d209cee4fadbcb2f8925ae14c6d9
Marko Manninen <elonmedia@gmail.com>
2017-04-11 17:17:08 +0300

    need jinja

--- hyml/ext.py
+++ hyml/ext.py
@@ -3,11 +3,15 @@
 import os
 from jinja2.ext import babel_extract as extract_jinja2
 
+jinja_extensions = '''
+                    hyml.ext.babel_extract, jinja2.ext.do, jinja2.ext.with_
+                   '''
+
 os.environ["VARIABLE_START_STRING"] = "(_ \""
 os.environ["VARIABLE_END_STRING"] = "\")"
 
 def babel_extract(fileobj, *args, **kw):
-    kw['options']['extensions'] = "hyml.ext.babel_extract"
+    kw['options']['extensions'] = jinja_extensions
     fileobj.read()
     raw_extract = extract_jinja2(fileobj, *args, **kw)
     fileobj.seek(0)
