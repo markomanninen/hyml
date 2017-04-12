efaac2797c670a65dc1a54127d3e1105c4ebab05
Marko Manninen <elonmedia@gmail.com>
2017-04-12 15:26:04 +0300

    rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -123,19 +123,26 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
+import sphinx_rtd_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': True,
+    'navigation_depth': 3,
+    'canonical_url' = 'http://hyml.readthedocs.io/en/latest/'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
