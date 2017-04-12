fa46bc53b518248cd593cca0ba34a5dcbb7b8040
Marko Manninen <elonmedia@gmail.com>
2017-04-12 15:35:02 +0300

    rtd should be default?

--- doc/conf.py
+++ doc/conf.py
@@ -123,12 +123,10 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-import sphinx_rtd_theme
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -142,7 +140,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
