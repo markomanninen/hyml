2953053c5f60d8b14b29a75a4fa07fae97e008eb
Marko Manninen <elonmedia@gmail.com>
2017-04-12 07:21:42 +0300

    debug round 3

--- hyml/ext.py
+++ hyml/ext.py
@@ -9,12 +9,13 @@ def extract_from_ast(source):
     def filter_hy(e):
         global d
         if isinstance(e, hy.HyExpression) or isinstance(e, list):
-            d = e[0]
+            if isinstance(e, hy.HyExpression):
+                d = e[0]
             return list(itertools.chain(*filter(None, map(filter_hy, e))))
         elif not isinstance(e, hy.HySymbol) and isinstance(e, hy.HyString):
             if d == hy.HySymbol("_") or \
                d == hy.HySymbol("gettext"):
-                return 0, str(d), e
+                return 0, str(d), str(e)
     return filter_hy(source)
 
 def chunks(l, n):
@@ -24,11 +25,10 @@ def chunks(l, n):
             yield tuple(t[:2]+[[t[2]]]+[[]])
 
 def babel_extract(fileobj, *args, **kw):
-    print([fileobj, args, kw])
+    print("pgettext" in args[0])
     byte = fileobj.read()
     source = "".join(map(chr, byte))
     node = hyi.import_buffer_to_hst(source)
     print(node)
-    chunk = chunks(extract_from_ast(node), 3)
-    print(list(chunk))
-    return chunk
+    print(list(chunks(extract_from_ast(node), 3)))
+    return chunks(extract_from_ast(node), 3)
