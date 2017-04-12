3f3d99f9116fab76b184f2774ef62be6c51a1e66
Marko Manninen <elonmedia@gmail.com>
2017-04-12 08:23:24 +0300

    removed linebreaks

--- hyml/ext.py
+++ hyml/ext.py
@@ -3,59 +3,30 @@
 # -*- coding: utf-8 -*-
 
 import hy, hy.importer as hyi
-
 from jinja2.ext import extract_from_ast
-
 import itertools
 
-
-
 def extract_from_ast(source):
-
     d = None
-
     def filter_hy(e):
-
         global d
-
         if isinstance(e, hy.HyExpression) or isinstance(e, list):
-
             if isinstance(e, hy.HyExpression):
-
                 d = e[0]
-
             return list(itertools.chain(*filter(None, map(filter_hy, e))))
-
         elif not isinstance(e, hy.HySymbol) and isinstance(e, hy.HyString):
-
-            if d == hy.HySymbol("_") or \
-
-               d == hy.HySymbol("gettext"):
-
+            if d == hy.HySymbol("_") or d == hy.HySymbol("gettext"):
                 return 0, str(d), str(e)
-
     return filter_hy(source)
 
-
-
 def chunks(l, n):
-
     if l:
-
         for i in range(0, len(l), n):
-
             t = l[i:i + n]
-
             yield tuple(t[:2]+[[t[2]]]+[[]])
 
-
-
 def babel_extract(fileobj, *args, **kw):
-
     byte = fileobj.read()
-
     source = "".join(map(chr, byte))
-
     node = hyi.import_buffer_to_hst(source)
-
     return chunks(extract_from_ast(node), 3)
