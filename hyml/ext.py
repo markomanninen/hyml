dd21b1e181f1a850a9ccd56843f4a6cabf3d5d19
Marko Manninen <elonmedia@gmail.com>
2017-04-12 08:25:02 +0300

    reset keyword

--- hyml/ext.py
+++ hyml/ext.py
@@ -13,7 +13,9 @@ def extract_from_ast(source):
         if isinstance(e, hy.HyExpression) or isinstance(e, list):
             if isinstance(e, hy.HyExpression):
                 d = e[0]
-            return list(itertools.chain(*filter(None, map(filter_hy, e))))
+            x = list(itertools.chain(*filter(None, map(filter_hy, e))))
+            d = None
+            return x
         elif not isinstance(e, hy.HySymbol) and isinstance(e, hy.HyString):
             if d == hy.HySymbol("_") or d == hy.HySymbol("gettext"):
                 return 0, str(d), str(e)
