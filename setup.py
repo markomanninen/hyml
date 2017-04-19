34d75bb6046746eea16a471e37334f279e3ad148
Marko Manninen <elonmedia@gmail.com>
2017-04-19 19:29:27 +0300

    lazy gettext fix

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ setup(
     'hyml': ['*.hy']
   },
   
-  version = 'v0.2.10',
+  version = 'v0.2.11',
   description = 'HyML - XML / (X)HTML generator for Hy',
   author = 'Marko Manninen',
   author_email = 'elonmedia@gmail.com',
 
   url = 'https://github.com/markomanninen/hyml',
-  download_url = 'https://github.com/markomanninen/hyml/archive/v0.2.10.tar.gz',
+  download_url = 'https://github.com/markomanninen/hyml/archive/v0.2.11.tar.gz',
   keywords = ['hylang', 'python', 'lisp', 'macros', 'markup language', 'dsl', 'xml', 'html', 'xhtml'],
   platforms = ['any'],
   
